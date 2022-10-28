3ea56dbeb9fc56945ed7d14de2ad33ea283218c1
Ben Harrison <53424421+benhar-dev@users.noreply.github.com>
2022-10-28 17:59:45 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# TwinCAT Runtime Compatibility Check (Beta)
+
+## Disclaimer
+This is a personal guide not a peer reviewed journal or a sponsored publication. We make
+no representations as to accuracy, completeness, correctness, suitability, or validity of any
+information and will not be liable for any errors, omissions, or delays in this information or any
+losses injuries, or damages arising from its display or use. All information is provided on an as
+is basis. It is the reader’s responsibility to verify their own facts.
+
+The views and opinions expressed in this guide are those of the authors and do not
+necessarily reflect the official policy or position of any other agency, organization, employer or
+company. Assumptions made in the analysis are not reflective of the position of any entity
+other than the author(s) and, since we are critically thinking human beings, these views are
+always subject to change, revision, and rethinking at any time. Please do not hold us to them
+in perpetuity.
+
+## Overview 
+This is a collection of powershell scripts used to identify common TwinCAT 3 incompatible settings in Windows.
+
+## Getting Started
+### Windows 7
+TBA
+
+### Windows 10
+Run Win10-Tc3-Check.ps1 as administrator
---
+++ src/Win10-Tc3-Check.ps1
@@ -0,0 +1,82 @@
+﻿# -------------------------------------------------------------------------------------------------------------------------------
+# Copyright 2022 benhar-dev
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
+# files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
+# modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software 
+# is furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE 
+# FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR 
+# IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# -------------------------------------------------------------------------------------------------------------------------------
+
+# This software is a work in progress
+
+cls
+
+function ConsoleLog-Title {
+	[CmdletBinding()]
+	param(
+		[Parameter()]
+		[string] $Info
+	)
+	Write-Host $Info
+    Write-Host ("-" * $Info.Length)
+}
+
+function ConsoleLog-Pass {
+	[CmdletBinding()]
+	param(
+		[Parameter()]
+		[string] $Info
+	)
+    Write-Host -NoNewline '['
+	Write-Host -ForegroundColor Green -NoNewLine ([Char]8730)
+    Write-Host -NoNewline '] '
+	Write-Host $Info
+}
+
+function ConsoleLog-Fail {
+	[CmdletBinding()]
+	param(
+		[Parameter()]
+		[string] $Info
+	)
+    Write-Host -NoNewline '['
+	Write-Host -ForegroundColor Red -NoNewLine 'X'
+    Write-Host -NoNewline '] '
+	Write-Host $Info
+}
+
+# ----------------------------------------------------------------------------
+
+ConsoleLog-Title "TwinCAT Runtime Compatibility Check (Beta)"
+
+$hypervheartbeat = Get-Service -name vmicheartbeat
+
+if ($hypervheartbeat.Status -contains 'Stopped') { 
+    ConsoleLog-Pass "Hyper-V Heartbeat service is stopped."
+}else {
+    ConsoleLog-Fail "Hyper-V Heartbeat service is running.  This indicates that Hyper-V is enabled."
+}
+
+
+# bcd check
+# ---------
+
+$bcd = bcdedit
+
+if ($bcd.Contains('disabledynamictick      Yes')) { 
+  ConsoleLog-Pass "Disable dynamic tick has been set."
+}else {
+  ConsoleLog-Fail "Disable dynamic tick has not been set. Please run C:\TwinCAT\3.1\System\win8settick.bat as Administrator"
+}
+
+# Windows feature check
+# ---------------------
+
+#Get-WindowsOptionalFeature -Online -FeatureName *Hyper-V*
