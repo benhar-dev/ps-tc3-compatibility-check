a5e439d6104d03b7854234c599bdb2dd38b160c5
Ben Harrison <53424421+benhar-dev@users.noreply.github.com>
2022-11-03 13:42:03 +1100

    finished refactor

--- src/Win10-Tc3-Check.ps1
+++ src/Win10-Tc3-Check.ps1
@@ -14,19 +14,22 @@
 # IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 # -------------------------------------------------------------------------------------------------------------------------------
 
+# ----------------------------------------------------------------------------
+# Forces the powershell script to run as Administrator
+# ----------------------------------------------------------------------------
+
 param([switch]$Elevated)
-function Test-Admin {
-    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-}
-if ((Test-Admin) -eq $false)  {
-    if ($elevated) {
-        # tried to elevate, did not work, aborting
-    } else {
+
+if ([UserInformation]::IsNotAdministrator())  {
+    if (-not $Elevated) {
         Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
     }
     exit
 }
+
+# ----------------------------------------------------------------------------
+# Helper Window Functions
+# ----------------------------------------------------------------------------
 
 function DisplayTitle {
 	[CmdletBinding()]
@@ -46,31 +49,6 @@ function DisplaySubTitle {
 	)
     Write-Host ""
 	Write-Host $subtitle
-}
-
-function ReportPass {
-	[CmdletBinding()]
-	param(
-		[Parameter()]
-		[string] $info
-	)
-    Write-Host -NoNewline '   ['
-	Write-Host -ForegroundColor Green -NoNewLine ([Char]8730)
-    Write-Host -NoNewline '] '
-	Write-Host $info
-}
-
-function ReportFail {
-	[CmdletBinding()]
-	param(
-		[Parameter()]
-		[string] $info
-	)
-    Write-Host -NoNewline '   ['
-	Write-Host -ForegroundColor Red -NoNewLine 'X'
-    Write-Host -NoNewline '] '
-	Write-Host $info
-
 }
 
 Function PauseWithMessage
@@ -95,44 +73,111 @@ Function PauseWithMessage
 }
 
 # ----------------------------------------------------------------------------
-
-
-Function GetTcVersion {
-    if (Test-Path 'HKLM:\SOFTWARE\WOW6432Node\Beckhoff\TwinCAT3\System') {
-        return [System.Version](Get-ItemProperty "HKLM:\SOFTWARE\WOW6432Node\Beckhoff\TwinCAT3\System").TcVersion
-    }
-}
-
-Function IsHypervisorHeartbeatServiceStopped {
-    if ($global:vmicheartbeatService -eq $null){
-        $global:vmicheartbeatService = Get-Service -name vmicheartbeat
-    }
-    return ($global:vmicheartbeatService.Status -contains 'Stopped')
-}
-
-Function IsVirtualizationVTXEnabledInBios {
-    if ($global:systemInfo -eq $null){
-        $global:systemInfo = systeminfo
-    }
-    return ($global:systemInfo -Like '*Virtualization Enabled In Firmware: Yes') 
-}
-
-Function IsWindowsFeatureDisabled($featureName) {
-    $feature = Get-WindowsOptionalFeature -FeatureName $featureName -Online
-    return ($feature.State -eq "Disabled")
-}
-
-Function AssertTrue($testName,$condition,$failMessage){
-
-
-}
-
+# Helper Classes
 # ----------------------------------------------------------------------------
-class UserLevel {
+
+class TwincatInformation {
+
+    static [System.Version]Version() {
+        if (Test-Path 'HKLM:\SOFTWARE\WOW6432Node\Beckhoff\TwinCAT3\System') {
+            return [System.Version](Get-ItemProperty "HKLM:\SOFTWARE\WOW6432Node\Beckhoff\TwinCAT3\System").TcVersion
+        }
+         return [System.Version]'0'
+    }
+    
+}
+
+class UserInformation {
 
     static [bool]IsAdministrator() {
         $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
         return $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    }
+
+     static [bool]IsNotAdministrator() {
+        return -Not [UserInformation]::IsAdministrator()
+    }
+    
+}
+
+class WindowsFeatureInformation {
+
+    static [bool]IsWindowsFeatureEnabled($featureName) {
+        $feature = Get-WindowsOptionalFeature -FeatureName $featureName -Online
+        return ($feature.State -eq "Enabled")
+    }
+
+    static [bool]IsWindowsFeatureDisabled($featureName) {
+        $feature = Get-WindowsOptionalFeature -FeatureName $featureName -Online
+        return ($feature.State -eq "Disabled")
+    }
+    
+}
+
+class SystemServiceInformation {
+
+    static [bool]IsServiceStopped($serviceName) {
+
+        $service = Get-Service -name $serviceName
+        return ($service.Status -contains 'Stopped')
+    }
+
+    static [bool]IsServiceRunning($serviceName) {
+
+        $service = Get-Service -name $serviceName
+        return ($service.Status -contains 'Running')
+    }
+    
+}
+
+class SystemInformation {
+
+    hidden static [System.Array]$output = $null
+
+    static SystemInformation(){
+        [SystemInformation]::Update()        
+    }
+
+    static Update() {
+        [SystemInformation]::output = Get-ComputerInfo | Get-Member | More
+    }
+
+    static [bool]IsVirtualisationEnabledInTheFirmware() {
+        return [SystemInformation]::output | Where-Object Name -eq 'HyperVRequirementVirtualizationFirmwareEnabled'
+    }
+    
+}
+
+class ProcessorInformation {
+
+    hidden static [PSCustomObject]$output = $null
+
+    static ProcessorInformation(){
+        [IntelProcessorInformation]::Update()        
+    }
+
+    static Update() {
+        [IntelProcessorInformation]::output = Get-CimInstance -Class CIM_Processor | Select-Object *
+    }
+
+    static [string]Name() {
+        return [IntelProcessorInformation]::output.Name
+    }
+    
+}
+
+class IntelProcessorInformation : ProcessorInformation {
+
+    static [bool]ProcessorIsIntel() {
+        return [IntelProcessorInformation]::Name() -like '*Intel*'
+    }
+    
+}
+
+class AmdProcessorInformation : ProcessorInformation {
+
+    static [bool]ProcessorIsAmdRyzen() {
+        return [IntelProcessorInformation]::Name() -like '*AMD Ryzen*'
     }
     
 }
@@ -174,83 +219,18 @@ class BootConfigurationData {
 
 }
 
-DisplayTitle "TwinCAT Runtime Compatibility Check (Beta)"
-DisplaySubTitle "Powershell checks"
+# ----------------------------------------------------------------------------
+# .Net Classes
+# ----------------------------------------------------------------------------
 
-    if ([UserLevel]::IsAdministrator()) { 
-        ReportPass "Script is running as Administrator."
-    }else {
-        ReportFail "Script not running as Administrator. Please run this script by right clicking and select 'Run as Administrator'"
-        PauseWithMessage('Unable to continue')
-        Exit
-    }
+$KernalInformationSourceCode = @"
+    namespace SystemInfo
+    {
+        using System;
+        using System.Runtime.InteropServices;
 
-DisplaySubTitle "Windows services checks"
-
-if (IsHypervisorHeartbeatServiceStopped) { 
-    ReportPass "Hyper-V Heartbeat service is stopped."
-}else {
-    ReportFail "Hyper-V Heartbeat service is running. This indicates that Hyper-V is enabled."
-}
-
-
-DisplaySubTitle "Bios checks"
-
-    if ([BootConfigurationData]::IsDynamicTickDisabled()) { 
-      ReportPass "Disable dynamic tick has been correctly set."
-    }else {
-      ReportFail "Disable dynamic tick has not been set. Please run C:\TwinCAT\3.1\System\win8settick.bat as Administrator"
-    }
-
-    if ([BootConfigurationData]::IsUsePlatformTickEnabled()) { 
-      ReportPass "Use platform tick has been correctly set."
-    }else {
-      ReportFail "Use platform tick has not been set. Please run C:\TwinCAT\3.1\System\win8settick.bat as Administrator"
-    }
-
-    # virtualization enabled in firmware check (VT-X)
-    if (IsVirtualizationVTXEnabledInBios) { 
-      ReportPass "Virtualization (VT-X) is enabled In Firmware."
-    }else {
-      ReportFail "Virtualization (VT-X) is disabled In Firmware."
-    }
-
-
-DisplaySubTitle "Windows feature checks"
-
-    # Windows feature Hyper-V
-    if(IsWindowsFeatureDisabled('Microsoft-Hyper-V-All')) {
-        ReportPass "Hyper-V Windows Feature is disabled."
-    } else {
-        ReportFail "Hyper-V Windows Feature is enabled. You will need to disable this using 'Turn Windows Features On or Off', and unticking Hyper-V"
-    }
-
-    # Windows feature Windows Sandbox
-    if(IsWindowsFeatureDisabled('Containers-DisposableClientVM')) {
-        ReportPass "Windows Sandbox Feature is disabled."
-    } else {
-        ReportFail "Windows Sandbox Feature is enabled. You will need to disable this using 'Turn Windows Features On or Off', and unticking Windows Sandbox"
-    }
-
-    # Windows feature Virtual Machine Platform
-    if(IsWindowsFeatureDisabled('VirtualMachinePlatform')) {
-        ReportPass "Virtual Machine Platform Feature is disabled."
-    } else {
-        ReportFail "Virtual Machine Platform Feature is enabled. You will need to disable this using 'Turn Windows Features On or Off', and unticking Virtual Machine Platform"
-    }
-
-DisplaySubTitle "Kernal checks"
-
-    # Kernal - bootDMAProtection
-    $bootDMAProtectionCheck =
-    @"
-      namespace SystemInfo
+        public static class KernalInformation
         {
-          using System;
-          using System.Runtime.InteropServices;
-
-          public static class NativeMethods
-          {
             internal enum SYSTEM_DMA_GUARD_POLICY_INFORMATION : int
             {
                 /// </summary>
@@ -259,47 +239,158 @@ DisplaySubTitle "Kernal checks"
 
             [DllImport("ntdll.dll")]
             internal static extern Int32 NtQuerySystemInformation(
-              SYSTEM_DMA_GUARD_POLICY_INFORMATION SystemDmaGuardPolicyInformation,
-              IntPtr SystemInformation,
-              Int32 SystemInformationLength,
-              out Int32 ReturnLength);
+                SYSTEM_DMA_GUARD_POLICY_INFORMATION SystemDmaGuardPolicyInformation,
+                IntPtr SystemInformation,
+                Int32 SystemInformationLength,
+                out Int32 ReturnLength);
 
-            public static byte BootDmaCheck() {
-              Int32 result;
-              Int32 SystemInformationLength = 1;
-              IntPtr SystemInformation = Marshal.AllocHGlobal(SystemInformationLength);
-              Int32 ReturnLength;
+            public static bool BootDmaEnabled() {
+                Int32 result;
+                Int32 SystemInformationLength = 1;
+                IntPtr SystemInformation = Marshal.AllocHGlobal(SystemInformationLength);
+                Int32 ReturnLength;
 
-              result = NativeMethods.NtQuerySystemInformation(
-                        NativeMethods.SYSTEM_DMA_GUARD_POLICY_INFORMATION.SystemDmaGuardPolicyInformation,
+                result = KernalInformation.NtQuerySystemInformation(
+                        KernalInformation.SYSTEM_DMA_GUARD_POLICY_INFORMATION.SystemDmaGuardPolicyInformation,
                         SystemInformation,
                         SystemInformationLength,
                         out ReturnLength);
 
-              if (result == 0) {
-                byte info = Marshal.ReadByte(SystemInformation, 0);
-                return info;
-              }
+                if (result == 0) {
+                    byte info = Marshal.ReadByte(SystemInformation, 0);
+                    return (info != 0);
+                }
 
-              return 0;
+                return false;
             }
-          }
         }
+    }
 "@
+Add-Type -TypeDefinition $KernalInformationSourceCode
 
-    Add-Type -TypeDefinition $bootDMAProtectionCheck
-    $bootDMAProtection = ([SystemInfo.NativeMethods]::BootDmaCheck()) -ne 0
+# ----------------------------------------------------------------------------
+# Test Functions
+# ----------------------------------------------------------------------------
 
-    if($bootDMAProtection) {
-        ReportPass "Kernel DMA Protection is on."
+function Test
+{
+  param
+  (
+    $name,
+  	$if = $true,
+  	$assertTrue = $true,
+    $assertFalse = $false,
+    $message = ''
+  )
+  if (-not $if) {return}
+
+  if (($assertTrue -eq $true) -and ($assertFalse -eq $false)){
+
+    ReportPass $name
+
+  } else {
+
+    ReportFail $name $message
+
+  }
+}
+
+function ReportPass {
+	[CmdletBinding()]
+	param(
+		[Parameter()]
+		[string] $info
+	)
+    Write-Host -NoNewline '   ['
+	Write-Host -ForegroundColor Green -NoNewLine ([Char]8730)
+    Write-Host -NoNewline ']PASS: '
+	Write-Host $info
+}
+
+function ReportFail {
+	[CmdletBinding()]
+	param(
+		[Parameter()]
+		[string] $info,
+        [string] $message
+	)
+    Write-Host -NoNewline '   ['
+	Write-Host -ForegroundColor Red -NoNewLine 'X'
+    Write-Host -NoNewline ']FAIL: '
+	
+
+    if ($message) {
+        Write-Host -NoNewline $info
+        Write-Host -NoNewline ' REASON: '
+        Write-Host $message
     } else {
-        ReportFail "Kernel DMA Protection is off."
+        Write-Host $info
     }
 
+}
+
+# ----------------------------------------------------------------------------
+# Start of compatibility checks
+# ----------------------------------------------------------------------------
+
+DisplayTitle "TwinCAT Runtime Compatibility Check (Beta)"
+DisplaySubTitle "Powershell checks"
+
+    Test 'Script is running as Administrator'`
+        -assertTrue ([UserInformation]::IsAdministrator())
+
+DisplaySubTitle "Windows services checks"
+
+    Test 'Hyper-V Heartbeat service is stopped'`
+        -assertTrue ([SystemServiceInformation]::IsServiceStopped('vmicheartbeat'))`
+        -message "Hyper-V Heartbeat service is running. This indicates that Hyper-V is enabled."
+
+DisplaySubTitle "Bios checks"
+
+    Test 'Dynamic Tick Disabled'`
+        -assertTrue ([BootConfigurationData]::IsDynamicTickDisabled())`
+        -message "Disable dynamic tick has not been set. Please run C:\TwinCAT\3.1\System\win8settick.bat as Administrator"
+    
+    Test 'Use Platform Tick Enabled'`
+        -assertTrue ([BootConfigurationData]::IsUsePlatformTickEnabled())`
+        -message "Use platform tick has not been set. Please run C:\TwinCAT\3.1\System\win8settick.bat as Administrator"
+
+    Test 'Virtualization (VT-X) is enabled In Firmware'`
+        -assertTrue ([SystemInformation]::IsVirtualisationEnabledInTheFirmware())`
+        -message "Virtualization (VT-X) is disabled In Firmware."
+
+DisplaySubTitle "Windows feature checks"
+
+    Test 'Hyper-V Windows Feature is disabled'`
+        -assertTrue ([WindowsFeatureInformation]::IsWindowsFeatureDisabled('Microsoft-Hyper-V-All'))`
+        -message "Hyper-V Windows Feature is enabled. You will need to disable this using 'Turn Windows Features On or Off', and unticking Hyper-V"
+
+    Test 'Windows Sandbox Feature is disabled'`
+        -assertTrue ([WindowsFeatureInformation]::IsWindowsFeatureDisabled('Containers-DisposableClientVM'))`
+        -message "Windows Sandbox Feature is enabled. You will need to disable this using 'Turn Windows Features On or Off', and unticking Windows Sandbox"
+
+    Test 'Virtual Machine Platform Feature is disabled'`
+        -assertTrue ([WindowsFeatureInformation]::IsWindowsFeatureDisabled('VirtualMachinePlatform'))`
+        -message "Virtual Machine Platform Feature is enabled. You will need to disable this using 'Turn Windows Features On or Off', and unticking Virtual Machine Platform"
+
+DisplaySubTitle "Kernal checks"
+
+    Test 'Kernel DMA Protection is off'`
+        -if ([TwincatInformation]::Version() -lt [System.Version]'3.1.4024.17')`
+        -assertFalse ([SystemInfo.KernalInformation]::BootDmaEnabled())`
+        -message "Kernel DMA Protection is on. This is only allowed with TwinCAT3 version 3.1.4024.17 and above"
+
+DisplaySubTitle "Processor checks"
+
+    Test 'Processor is compatible'`
+        -if([AmdProcessorInformation]::ProcessorIsAmdRyzen())`
+        -assertTrue([TwincatInformation]::Version() -ge [System.Version]'3.1.4024.25')`
+        -message "AMD Ryzan Detected. This is only allowed with TwinCAT3 version 3.1.4024.25 and above"
+
+    # TODO: check required, 11th Gen Intel supported from 4024.22
+
+    # TODO: check required, 12th Gen Intel supported from 4024.32 or 4024.35, needs confirming
+
 DisplaySubTitle "Checks Complete"
-
-
-
-
 PauseWithMessage('Done')
 Exit
